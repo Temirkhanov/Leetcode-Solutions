0723b9b0a086d90414fc44f9948773ef86ad00eb
Ruslan Temirkhanov <rusl0424@gmail.com>
2019-09-13 20:48:32 -0400

    Added -XOR- solution for SingleNumber in Arrays.

--- src/Arrays/SingleNumber.java
+++ src/Arrays/SingleNumber.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 public class SingleNumber {
 
-    public int singleNumber(int[] nums) {
+    public static int singleNumber(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int num : nums)
             if (set.contains(num))
@@ -14,5 +14,30 @@ public class SingleNumber {
         int result = set.iterator().next();
         return result;
     }
+
+    // XOR solution
+    public static int singleNum(int[] nums){
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++){
+            System.out.print(res + " XOR " + nums[i] + " = ");
+            res = res ^ nums[i];
+            System.out.print(res + "\n");
+        }
+        return res;
+    }
+
+    public static void printArr(int[] a){
+        for(int b : a)
+            System.out.print(b + ", ");
+        System.out.println("\n--------------");
+    }
+
+
+    public static void main(String[] args){
+        int[] a = {2, 3, 5, 4, 5, 3, 4};
+        printArr(a);
+        System.out.println("Single int -> " + singleNum(a));
+    }
+
 
 }
